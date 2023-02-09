76830ff25d027397c5f7c9268959288bfbb3a092
Tomer <tomerfry@gmail.com>
2023-02-09 10:31:17 +0200

    Changed a bit.

--- 0/main.c
+++ 0/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
-#include <socket.h>
+#include <sys/socket.h>
 
 
-int get_input_from_socket(int sock) {
+int get_url_from_socket(int sock) {
 	unsigned int size;
 	char *input;
+	char c;
 	
 	if (recv(sock, &size, sizeof(size), 0) < 0)
 		return -1;
@@ -13,9 +15,18 @@ int get_input_from_socket(int sock) {
 	input = malloc(size);
 
 	for (int i = 0; i < size; i++) {
-		if (recv(sock, &input[i], sizeof(char), 0) < 0) {
+		if (recv(sock, &c, sizeof(char), 0) < 0) {
 			free(input);
 			return -1;
+		}
+		char temp =  isalnum(c) || c == '.' || c == '?' || c == '_' || c == '-' || c == '#' || c == ';' ? c : (c == ' ' ? '+' : 0);
+		if (!temp) {
+			char *encoded = malloc(3);
+			sprintf(encoded, "%%%02X", c);
+			strcat(input, encoded);
+		}
+		else {
+			strcat(input, &temp);
 		}
 	}	
 	
